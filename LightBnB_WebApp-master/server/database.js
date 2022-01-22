5a81d61f022914399236f32b13c54494ad56bd55
Reshawn <reshawnhamilton97@gmail.com>
2022-01-22 23:26:45 +0000

    added property queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -80,12 +80,36 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function (options, limit = 10) {
 
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+  const getAllProperties = function (options, limit = 10) {
+    // 1
+    const queryParams = [];
+    // 2
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    // 3
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+  
+    // 4
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    // 5
+    console.log(queryString, queryParams);
+  
+    // 6
+    return pool.query(queryString, queryParams).then((res) => res.rows);
+  };
 }
 exports.getAllProperties = getAllProperties;
 
@@ -96,9 +120,39 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
+
+  db.query(`INSERT INTO properties
+  (owner_id ,
+    title,
+    description ,
+    thumbnail_photo_url ,
+    cover_photo_url ,
+    cost_per_night ,
+    street ,
+    city ,
+    province ,
+    post_code ,
+    country ,
+    parking_spaces ,
+    number_of_bathrooms ,
+    number_of_bedrooms 
+  ) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)`,
+  [property.owner_id ,
+    property.title,
+    property.description ,
+    property.thumbnail_photo_url ,
+    property.cover_photo_url ,
+    property.cost_per_night ,
+    property.street ,
+    property.city ,
+    property.province ,
+    property.post_code ,
+    property.country ,
+    property.parking_spaces ,
+    property.number_of_bathrooms ,
+    property.number_of_bedrooms ]
+  )};
+
   return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
